bcc330df94c41bfef1a4bbf6d51ad896992dca50
overflow64 <luca.ted79@gmail.com>
2023-03-09 12:00:26 +0100

    primo commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test prima linea</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, explicabo. Voluptas eum porro mollitia facere delectus earum nemo error illum ullam? Facere assumenda iure quas molestias illum numquam excepturi nisi?
+    </p>
+</body>
+</html>
